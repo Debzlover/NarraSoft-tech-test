9d1a1e4b8050b9c93fbef171b60d900dc9697663
bonix <debonerdulos@gmail.com>
2022-12-02 13:54:08 +0800

    disable console logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { ModalContextProvider } from '../helpers/context/modalContext'
 import { DropedToolProvider } from '../helpers/context/dropedToolsContext'
 
 // temporary disable all logs
-// console.log = () => {}
+console.log = () => {}
 console.warn = () => {}
 
 export default function App({ Component, pageProps }: AppProps) {
